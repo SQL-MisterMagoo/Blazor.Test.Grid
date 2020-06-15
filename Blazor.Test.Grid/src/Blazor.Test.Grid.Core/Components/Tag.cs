65d7f8d7977097c1172e19df7e87cad0376f5fcb
Mister Magoo <mister.magoo+githubsql@gmail.com>
2020-06-15 16:01:40 +0100

    Tidy Before Name Change

---
+++ Blazor.Test.Grid/src/Blazor.Test.Grid.Core/Components/Tag.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.CompilerServices;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Collections.Generic;
+
+namespace Blazor.Test.Grid.Core.Components
+{
+    public partial class Tag : ComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public string Name { get; set; }
+        [Parameter(CaptureUnmatchedValues =true)] public Dictionary<string,object> attributes { get; set; }
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(1, Name);
+            builder.AddMultipleAttributes(2, RuntimeHelpers.TypeCheck<IEnumerable<KeyValuePair<string, object>>>(attributes));
+            ChildContent(builder);
+            builder.CloseElement();
+        }
+    }
+}
--- Blazor.Test.Grid/src/Blazor.Test.Grid.Core/GridRowTypes.cs
+++ Blazor.Test.Grid/src/Blazor.Test.Grid.Core/GridRowTypes.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Blazor.Test.Grid.Core.Components
+namespace Blazor.Test.Grid.Core
 {
     public enum GridRowTypes
     {
